a612e7f9bd10770ae73a96d02e3a7d6e9c3607ed
cazinge <eddie.azinge@gmail.com>
2016-11-06 12:56:01 -0800

    diverged files

---
+++ src/indexWithEventLoop.js
@@ -0,0 +1,75 @@
+// This code sample shows how to call and receive external rest service data, within your skill Lambda code.
+
+// var AWS = require('aws-sdk');
+
+var https = require('https');
+
+exports.handler = function( event, context ) {
+    var say = "";
+    var shouldEndSession = false;
+    var sessionAttributes = {};
+    var myState = "";
+    var myType = "";
+    var myAnswer = "";
+    var pop = 0;
+    var rank = 0;
+
+    if (event.session.attributes) {
+        sessionAttributes = event.session.attributes;
+    }
+
+    if (event.request.type === "LaunchRequest") {
+        say = "Welcome to Quizlex! Would you like to learn or test?";
+        context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession) });
+
+    } else {
+        var IntentName = event.request.intent.name;
+
+        if (IntentName === "TypeIntent") {
+            if (event.request.intent.slots.typeoption.value) {
+                myType = event.request.intent.slots.typeoption.value;
+                say = "Time to " + myType;
+                context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession)});
+            }
+        } else if (IntentName === "AnswerIntent") {
+            if (event.request.intent.slots.answeroption.value) {
+                myAnswer = event.request.intent.slots.answeroption.value;
+
+            }
+        }
+
+
+        } else if (IntentName === "AMAZON.StopIntent" || IntentName === "AMAZON.CancelIntent") {
+            say = "You asked for " + sessionAttributes.requestList.toString() + ". Thanks for playing!";
+            shouldEndSession = true;
+            context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession) });
+
+
+        } else if (IntentName === "AMAZON.HelpIntent" ) {
+            say = "Just say the name of a U.S. State, such as Massachusetts or California."
+            context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession) });
+
+        }
+    }
+};
+
+function buildSpeechletResponse(say, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "SSML",
+            ssml: "<speak>" + say + "</speak>"
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "SSML",
+                ssml: "<speak>Please try again. " + say + "</speak>"
+            }
+        },
+        card: {
+            type: "Simple",
+            title: "My Card Title",
+            content: "My Card Content, displayed on the Alexa App or alexa.amazon.com"
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
